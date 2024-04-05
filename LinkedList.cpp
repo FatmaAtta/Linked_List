6928fbeab8257dfbfb9a79dbda2015b3f3b38c6c
FatmaAtta <fatmamaali@gmail.com>
2024-04-05 20:12:11 +0200

    added some methods

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 namespace LinkedList{
-
+    LinkedList::LinkedList():head(nullptr),last(nullptr),count(0){}
+    DoublyLinkedList::DoublyLinkedList():head(nullptr),last(nullptr),count(0){}
+    SNode::SNode(int data):data(data){}
+    DNode::DNode(int data):data(data){}
+    void LinkedList::pushBack(int num){
+        SNode *newNode = new SNode(num);
+        newNode->next=nullptr;
+        
+    }
+    void DoublyLinkedList::pushBack(int num){
+        DNode *newNode = new DNode(num);
+        newNode->next=nullptr;
+        newNode->prev=last;
+        last=newNode;
+    }
 }
--- LinkedList.h
+++ LinkedList.h
@@ -5,7 +5,55 @@
 using namespace std;
 
 namespace LinkedList{
+    class SNode{
+    public:
+        int data;
+        SNode* next;
+        SNode(int data);
+    };
 
+    class DNode{
+    public:
+        int data;
+        DNode* next;
+        DNode* prev;
+        DNode(int data);
+    };
+
+    class LinkedList{
+    public:
+        int count;
+        SNode *head;
+        SNode *last;
+        void pushBack(int num);
+        void pushFront(int num);
+        void popFront(int num);
+        void popBack(int num);
+        void insertMiddle(int num, int index);
+        bool isEmpty();
+        int size();
+        void printList();
+        LinkedList();
+    };
+
+    class DoublyLinkedList{
+        int count;
+        DNode *head;
+        DNode *last;
+        void pushBack(int num);
+        void pushFront(int num);
+        void insertMiddle(int num, int index);
+        void popFront(int num);
+        void popBack(int num);
+        bool isEmpty();
+        int size();
+        void printList();
+        DoublyLinkedList();
+    };
+
+    class CircularLinkedList{
+
+    };
 }
 
 #endif
